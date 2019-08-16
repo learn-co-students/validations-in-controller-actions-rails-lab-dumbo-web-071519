a81ef9c885c1b3cd710a56cc500c8bc7efa75fcd
Alessandra Vertrees <avertrees447@gmail.com>
2019-08-16 08:38:23 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, uniqueness: true
+#     1. Add validations to `Author` such that...
+#   1. The name cannot be blank
+#   1. The e-mail is unique
 end
